211ebfde63e85790566b76392ac31cb677d07f82
Prasurjyasarma <82230151+Prasurjyasarma@users.noreply.github.com>
2021-05-08 16:30:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,81 @@
 'use strict';
-//number
-const b = Math.trunc(Math.random() * 20) + 1;
+//random number
+let b = Math.trunc(Math.random() * 20) + 1;
+
+console.log(b);
 let sc = 20;
+let highScore = 0;
+
+//value
 document.querySelector('.check').addEventListener('click', function () {
   const gs = Number(document.querySelector('.guess').value);
 
+  //function of message
+  const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+  };
+
+  //Background colour
+  const backColour = function (colour) {
+    document.querySelector('body').style.backgroundColor = colour;
+  };
+
+  //no input
   if (!gs) {
-    document.querySelector('.message').textContent = 'Not a Number';
+    displayMessage('Not a Number');
   }
+
   //Correct Number
   else if (gs === b) {
-    document.querySelector('.message').textContent =
-      '🏆🏆 Right Number WOOOOHHHW';
+    displayMessage('🏆🏆 Right Number WOOOOHHHW');
     document.querySelector('.number').textContent = b;
-    document.querySelector('.highscore').textContent = sc;
-    document.querySelector('body').style.backgroundColor = '#60b347';
+    backColour('#60b347');
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.head').textContent = 'CORRECT GUESS';
     document.querySelector('.heading').textContent = 'CORRECT GUESS';
-  }
-  //Number low
-  else if (gs < b) {
-    if (sc > 0) {
-      document.querySelector('.message').textContent = '😕 Too low';
-      document.querySelector('body').style.backgroundColor = '#708090';
-      sc--;
-      document.querySelector('.score').textContent = sc;
-    } else {
-      document.querySelector('.message').textContent = 'Try again';
-      document.querySelector('body').style.backgroundColor = '#ff0000';
-      document.querySelector('.head').textContent = 'YOU LOST THE GAME';
-      document.querySelector('.heading').textContent = 'YOU LOST THE GAME';
+    if (sc > highScore) {
+      highScore = sc;
+      document.querySelector('.highscore').textContent = highScore;
     }
   }
-  //Number high
-  else if (gs > b) {
+
+  //Different case :
+  else if (gs !== b) {
     if (sc > 0) {
-      document.querySelector('.message').textContent = '😕 Too high';
-      document.querySelector('body').style.backgroundColor = '#FF4500';
+      if (gs < b) {
+        displayMessage('👇 Too low');
+        backColour('#778899');
+      }
+      if (gs > b) {
+        displayMessage('☝ Too high');
+        backColour('#FF4500');
+      }
       sc--;
       document.querySelector('.score').textContent = sc;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game';
-      document.querySelector('body').style.backgroundColor = '#ff0000';
+      displayMessage('Try again');
+      backColour('#ff0000');
       document.querySelector('.head').textContent = 'YOU LOST THE GAME';
       document.querySelector('.heading').textContent = 'YOU LOST THE GAME';
     }
   }
 });
+
 //Relode the page :
 document.querySelector('.again').addEventListener('click', function () {
+  sc = 20;
+  b = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.head').textContent = 'Guess My Number';
+  displayMessage('Start guessing');
+  document.querySelector('.heading').textContent = 'Guess My Number!';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.guess').value = '';
+});
+
+//reset the page
+document.querySelector('.reset').addEventListener('click', function () {
   location.reload();
 });
